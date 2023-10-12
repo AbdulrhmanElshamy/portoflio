9d9558599b2d66336b79900d7018d39d64a97b25
AbDulrhman Elshamy <AbdulrhmanElshamy12@gmail.com>
2023-10-12 08:48:46 +0200

    gedo

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    // Transition effect for navbar 
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+
+        if ($(this).scrollTop() > 500) {
+            $('nav').addClass('nav-solid');
+            $('.bi-rocket-takeoff').css("right", "1%");
+            $('.bi-rocket-takeoff').css("display", "block");
+            $('#AboutPic img').css("transform", "translateX(18%)");
+
+        } else {
+            $('nav').removeClass('nav-solid');
+            $('.bi-rocket-takeoff').css("right", "-5%");
+            $('.bi-rocket-takeoff').css("display", "none");
+            $('.bi-rocket-takeoff').css("bottom", "2%");
+            $('.bi-rocket-takeoff').css("font-size", "40px");
+            $('#AboutPic img').css("transform", "translateX(87%)");
+
+
+        }
+    });
+    $('.bi-rocket-takeoff').on('click', function () {
+        $('.bi-rocket-takeoff').css("bottom", "90%");
+        $('.bi-rocket-takeoff').css("font-size", "0px");
+        $('body, html').animate({
+            scrollTop: $("#Home").offset().top
+        }, 600);
+    });
+});
+
